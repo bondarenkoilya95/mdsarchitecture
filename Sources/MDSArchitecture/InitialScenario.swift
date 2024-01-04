51d3fba96093a362d6132a48328c5afa4a05355d
bondarenkoilya95 <bondarenko.ilya.95@gmail.com>
2024-01-05 00:30:12 +0200

    Initial Commit

---
+++ Sources/MDSArchitecture/InitialScenario.swift
@@ -0,0 +1,12 @@
+//
+//  InitialScenario.swift
+//  
+//
+//  Created by Ilya Bondarenko on 05.01.2024.
+//
+
+import UIKit
+
+class InitialScenario: Scenario {
+
+}
---
+++ Sources/MDSArchitecture/Scenario.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ilya Bondarenko on 05.01.2024.
+//
+
+import Foundation
